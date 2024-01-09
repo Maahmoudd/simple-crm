e368b707f0dfe4e0a926a0fbcbea9a8f64764a65
Mahmoud EL-Sabahy <73112243+Maahmoudd@users.noreply.github.com>
2024-01-10 01:11:10 +0200

    Update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,44 +1,28 @@
-<?php
-
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\NotesController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('customers')->group(function () {
+        Route::get('{id}', [CustomerController::class, 'show'])->name('customers.show');
+        Route::patch('{id}', [CustomerController::class, 'update'])->name('customers.update');
+        Route::delete('{id}', [CustomerController::class, 'delete'])->name('customers.delete');
+        Route::post('export', [CustomerController::class, 'export'])->name('customers.export');
+        Route::post('/', [CustomerController::class, 'create'])->name('customers.create');
+        Route::get('/', [CustomerController::class, 'index'])->name('customers.index');
 
-    Route::get('customers/{id}', [CustomerController::class,'show']);
-    Route::patch('customers/{id}', [CustomerController::class,'update']);
-    Route::delete('customers/{id}', [CustomerController::class,'delete']);
-    Route::post('customers/export', [CustomerController::class,'export']);
-    Route::post('customers', [CustomerController::class,'create']);
-    Route::get('customers', [CustomerController::class,'index']);
+        Route::prefix('{customerId}/notes')->group(function () {
+            Route::get('{id}', [NotesController::class, 'show'])->name('notes.show');
+            Route::patch('{id}', [NotesController::class, 'update'])->name('notes.update');
+            Route::delete('{id}', [NotesController::class, 'delete'])->name('notes.delete');
+            Route::post('/', [NotesController::class, 'create'])->name('notes.create');
+            Route::get('/', [NotesController::class, 'index'])->name('notes.index');
+        });
 
-
-    Route::get('customers/{customerId}/notes/{id}', [NotesController::class,'show']);
-    Route::patch('customers/{customerId}/notes/{id}', [NotesController::class,'update']);
-    Route::delete('customers/{customerId}/notes/{id}', [NotesController::class,'delete']);
-    Route::post('customers/{customerId}/notes', [NotesController::class,'create']);
-    Route::get('customers/{customerId}/notes', [NotesController::class,'index']);
-
-    //Route::get('customers/{customerId}/projects/{id}', [ProjectController::class,'show']);
-    //Route::patch('customers/{customerId}/projects/{id}', [ProjectController::class,'update']);
-    //Route::delete('customers/{customerId}/projects/{id}', [ProjectController::class,'delete']);
-    Route::post('customers/{customerId}/projects', [ProjectController::class,'createProject']);
-    //Route::get('customers/{customerId}/projects', [ProjectController::class,'index']);
-
+        Route::post('{customerId}/projects', [ProjectController::class, 'createProject'])->name('projects.create');
+    });
 });
 
-Route::post('users', [UserController::class,'create'] );
+Route::post('users', [UserController::class, 'create'])->name('users.create');
